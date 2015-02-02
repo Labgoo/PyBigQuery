db3fa0fd0fc65c2d9c6ed888ba10ca63e7dbfa18
Neta Krakover <neta@wondermall.com>
2015-02-02 13:47:09 -0800

    Add options to authentication from auth_token and gee

--- bigquery/client.py
+++ bigquery/client.py
@@ -1,3 +1,10 @@
+import os
+from googleapiclient.discovery import build
+import httplib2
+from oauth2client import gce
+from oauth2client.appengine import AppAssertionCredentials
+from oauth2client.file import Storage
+
 __author__ = 'ekampf'
 
 
@@ -64,7 +71,7 @@ class BigQueryHttp(apiclient_request.HttpRequest):
 
 
 class BigQueryClient(object):
-    def __init__(self, use_jwt_credentials_auth=False, jwt_account_name='', jwt_key_func=None, trace=None):
+    def __init__(self, use_jwt_credentials_auth=False, jwt_account_name='', jwt_key_func=None, oauth_credentails_file=None, trace=None):
         """
         :param trace: A value to add to all outgoing requests
         :return:
@@ -74,6 +81,7 @@ class BigQueryClient(object):
         self.use_jwt_credentials_auth = use_jwt_credentials_auth
         self.jwt_account_name = jwt_account_name
         self.jwt_key_func = jwt_key_func
+        self.oauth_credentails_file = oauth_credentails_file
 
     ###### Wrapping BigQuery's API
 
@@ -92,26 +100,62 @@ class BigQueryClient(object):
     def tables(self):
         return self.api_client.tables()
 
+    def get_http_for_request(self):
+        if self.use_jwt_credentials_auth:  # Local debugging using pem file
+            scope = 'https://www.googleapis.com/auth/bigquery'
+            from oauth2client.client import SignedJwtAssertionCredentials
+            credentials = SignedJwtAssertionCredentials(self.jwt_account_name, self.jwt_key_func(), scope=scope)
+            logging.info("Using Standard jwt authentication")
+            return credentials.authorize(httplib2.Http())
+
+        elif self.is_in_appengine():  # App engine
+            from google.appengine.api import memcache
+            scope = 'https://www.googleapis.com/auth/bigquery'
+            credentials = AppAssertionCredentials(scope=scope)
+            logging.info("Using Standard appengine authentication")
+            return credentials.authorize(httplib2.Http(memcache))
+
+        elif self.oauth_credentails_file:  # Local oauth token
+            http = httplib2.Http()
+            cred_file = self.oauth_credentails_file
+            storage = Storage(cred_file)
+            credentials = storage.get()
+            if not credentials:
+                raise EnvironmentError('No credential file present')
+            http = credentials.authorize(http)
+            credentials.refresh(http)
+            logging.info("Using Standard OAuth authentication")
+            return http
+
+        elif self.is_in_gce_machine():  # GCE authorization
+            http = httplib2.Http()
+            credentials = gce.AppAssertionCredentials('')
+            http = credentials.authorize(http)
+            credentials.refresh(http)
+            logging.info("Using GCE authentication")
+            return http
+
+    @staticmethod
+    def is_in_appengine():
+        'SERVER_SOFTWARE' in os.environ and os.environ['SERVER_SOFTWARE'].startswith('Google App Engine/')
+
+    @staticmethod
+    def is_in_gce_machine():
+        try:
+            metadata_uri = 'http://metadata.google.internal'
+            http = httplib2.Http()
+            http.request(metadata_uri, method='GET')
+            return True
+        except httplib2.ServerNotFoundError:
+            return False
+
+
     @property
     def api_client(self):
-        import httplib2
-        from apiclient.discovery import build
-        from oauth2client.appengine import AppAssertionCredentials
-        from google.appengine.api import memcache
-
-        scope = 'https://www.googleapis.com/auth/bigquery'
-
-        if self.use_jwt_credentials_auth:
-            from libs.oauth2client.client import SignedJwtAssertionCredentials
-            credentials = SignedJwtAssertionCredentials(self.jwt_account_name, self.jwt_key_func(), scope=scope)
-        else:
-            credentials = AppAssertionCredentials(scope=scope)
-
-        http = credentials.authorize(httplib2.Http(memcache))
-
         bigquery_model = BigQueryModel(trace=self.trace)
         bigquery_http = BigQueryHttp.factory(bigquery_model)
 
+        http = self.get_http_for_request()
         return build("bigquery", "v2", http=http, model=bigquery_model, requestBuilder=bigquery_http)
 
 
